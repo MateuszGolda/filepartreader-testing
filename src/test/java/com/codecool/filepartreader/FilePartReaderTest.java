b19aaee4af8931c7d49a06ead41fc46773569a7c
MateuszGolda <mateusz.golda1993@gmail.com>
2020-08-27 17:49:33 +0200

    add tests

--- src/test/java/com/codecool/filepartreader/FilePartReaderTest.java
+++ src/test/java/com/codecool/filepartreader/FilePartReaderTest.java
@@ -1,12 +1,69 @@
 package com.codecool.filepartreader;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
 
-public class FilePartReaderTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilePartReaderTest {
+    FilePartReader reader;
+
+    @BeforeEach
+    void setReader() {
+        reader = new FilePartReader();
+        reader.setup("src/test/java/com/codecool/filepartreader/text.txt", 1, 1);
+    }
+
+    @AfterEach
+    void destroyReader() {
+        reader = null;
+    }
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void should_ReturnFileContent_ForTestFile() throws IOException {
+        String fileContent = reader.read();
+        assertEquals("Line 1 some more words for sorting Rotator\nLine 2\nLine 3\nLine 4 deified, civic, radar, level, rotor, kayak, reviver", fileContent, "The read method should return all file lines as String");
+    }
+
+    @Test
+    void should_ReturnGivenLinesOfFile_ForTestFile() {
+        assertAll(
+                () -> assertEquals("Line 1 some more words for sorting Rotator\n", reader.readLines()),
+                () -> {
+                    reader.setup("src/test/java/com/codecool/filepartreader/text.txt", 2, 2);
+                    assertEquals("Line 2\n", reader.readLines());
+                },
+                () -> {
+                    reader.setup("src/test/java/com/codecool/filepartreader/text.txt", 1, 4);
+                    assertEquals("Line 1 some more words for sorting Rotator\n" +
+                            "Line 2\n" +
+                            "Line 3\n" +
+                            "Line 4 deified, civic, radar, level, rotor, kayak, reviver\n", reader.readLines());
+                },
+                () -> {
+                    reader.setup("src/test/java/com/codecool/filepartreader/text.txt", 1, 10);
+                    assertEquals("Line 1 some more words for sorting Rotator\n" +
+                            "Line 2\n" +
+                            "Line 3\n" +
+                            "Line 4 deified, civic, radar, level, rotor, kayak, reviver\n", reader.readLines());
+                }
+        );
+    }
+
+    @Test
+    void should_ThrowIOException_ForWrongFileName() {
+        reader.setup("src/test/java/com/codecool/filepartreader/a.txt", 1, 1);
+
+        assertThrows(IOException.class, () -> {
+            reader.read();
+        });
+    }
+
+    @Test
+    void should_ThrowIllegalArgumentException_ForNegativeFromLine() {
+        assertThrows(IllegalArgumentException.class, () -> reader.setup("src/test/java/com/codecool/filepartreader/text.txt", -1, 1));
     }
 }
